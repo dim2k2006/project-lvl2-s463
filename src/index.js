d1ed511985a4f22798eb2d9bb8860b4d04229e85
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-16 14:36:28 +0300

    Add "genDiff" function

--- src/index.js
+++ src/index.js
@@ -10,28 +10,46 @@ import has from 'lodash/has';
 const genDiff = (pathToFile1, pathToFile2) => {
   const data1 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', pathToFile1)));
   const data2 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', pathToFile2)));
+  const data = [
+    ...Object.keys(data1).map(key => ({ key, value: data1[key] })),
+    ...Object.keys(data2).map(key => ({ key, value: data2[key] })),
+  ];
 
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
+  return `{\n${
+    data
+      .reduce((accumulator, item) => {
+        const { key, value } = item;
 
-  const iter = (index, accumulator) => {
-    const key1 = keys1[index];
-    const key2 = keys2[index];
+        const existingItem = accumulator.find(accItem => accItem.key === key);
 
-    if (!key1 && !key2) return accumulator;
+        if (!existingItem && (data1[key] === data2[key])) return [...accumulator, { key, value, action: ' ' }];
 
-    if (!has(data2, key1)) return iter(index + 1, `${accumulator}\n  - ${key1}: ${data1[key1]}`);
+        if (!existingItem && (has(data1, key) && has(data2, key) && (data1[key] !== data2[key]))) return [...accumulator, { key, value, action: '+' }];
 
-    /* 1 */ if (has(data2, key1) && data1[key1] !== data2[key1]) return iter(index + 1, `${accumulator}\n  + ${key1}: ${data2[key1]}\n  - ${key1}: ${data1[key1]}`);
+        if (!existingItem && (has(data1, key) && !has(data2, key))) return [...accumulator, { key, value, action: '-' }];
 
-    /* 2 */ if (!has(data1, key2)) return iter(index + 1, `${accumulator}\n  + ${key2}: ${data2[key2]}`);
+        if (!existingItem && data1[key] && !data2[key]) return [...accumulator, { key, value, action: '-' }];
 
-    // объеденить 1 и 2
+        if (!existingItem && !data1[key] && data2[key]) return [...accumulator, { key, value, action: '+' }];
 
-    return iter(index + 1, `${accumulator}\n    ${key1}: ${data1[key1]}`);
-  };
+        const existingItemIndex = accumulator.findIndex(accItem => accItem.key === key);
 
-  return `{${iter(0, '')}\n}`;
+        if (existingItem && existingItem.value !== value) {
+          return [
+            ...accumulator.slice(0, existingItemIndex),
+            { key, value, action: '+' },
+            { key, value: existingItem.value, action: '-' },
+            ...accumulator.slice(existingItemIndex + 1),
+          ];
+        }
+
+        if (existingItem && existingItem.value === value) return accumulator;
+
+        return accumulator;
+      }, [])
+      .map(item => `${item.action} ${item.key}: ${item.value}`)
+      .join('\n')
+  }\n}\n`;
 };
 
 export default genDiff;

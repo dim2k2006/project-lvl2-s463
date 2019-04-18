45957a8c2b924b933e28da7fe386e0da8ab0067e
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-18 21:03:27 +0300

    Update bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import genDiff from '..';
 
 program
   .version('1.0.0')
   .arguments('<firstConfig> <secondConfig>')
+  .action((path1, path2) => {
+    console.log(genDiff(path1, path2));
+  })
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);

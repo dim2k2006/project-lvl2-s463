70f74e4940e84b6dd1624c293a92d3c0c6b0f6f5
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-20 16:04:35 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,16 +4,18 @@ import genDiff from '../src';
 
 const fixturesPath = path.join('.', '__tests__', '__fixtures__');
 
+const expectedFlat = fs.readFileSync(path.resolve(fixturesPath, 'expected.txt'), 'utf8');
+const expectedNested = fs.readFileSync(path.resolve(fixturesPath, 'expected-nested.txt'), 'utf8');
+
 const fixtures = [
-  [path.join(fixturesPath, 'before.json'), path.join(fixturesPath, 'after.json')],
-  [path.join(fixturesPath, 'before.yml'), path.join(fixturesPath, 'after.yml')],
-  [path.join(fixturesPath, 'before.ini'), path.join(fixturesPath, 'after.ini')],
+  [path.join(fixturesPath, 'before.json'), path.join(fixturesPath, 'after.json'), expectedFlat],
+  [path.join(fixturesPath, 'before.yml'), path.join(fixturesPath, 'after.yml'), expectedFlat],
+  [path.join(fixturesPath, 'before.ini'), path.join(fixturesPath, 'after.ini'), expectedFlat],
+  [path.join(fixturesPath, 'before-nested.json'), path.join(fixturesPath, 'after-nested.json'), expectedNested],
 ];
 
 describe('genDiff', () => {
-  const expected = fs.readFileSync(path.resolve(__dirname, '__fixtures__', 'expected.txt'), 'utf8');
-
-  test.each(fixtures)('Should return correct diff. Index of the test case: %#', (before, after) => {
+  test.each(fixtures)('Should return correct diff. Index of the test case: %#', (before, after, expected) => {
     expect(genDiff(before, after)).toBe(expected);
   });
 });

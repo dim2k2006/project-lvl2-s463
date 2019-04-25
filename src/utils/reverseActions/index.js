39168dc1725a1c85738e342bc2c1adbe0d0eeddb
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-25 22:16:07 +0300

    Add "reverseActions" util function

---
+++ src/utils/reverseActions/index.js
@@ -0,0 +1,23 @@
+import actionTypes from '../actionTypes';
+
+/**
+ * Reverses actions in ast tree
+ * @param {Array} ast
+ * @returns {Array}
+ */
+const reverseActions = ast => ast.map((item) => {
+  const { action = actionTypes.DEFAULT, children = [] } = item;
+  const processedChildren = reverseActions(children);
+
+  if (action === actionTypes.DEFAULT) return { ...item, children: processedChildren };
+
+  return {
+    ...item,
+    action: (action === actionTypes.SUBTRACTION)
+      ? actionTypes.ADDITION
+      : actionTypes.SUBTRACTION,
+    children: processedChildren,
+  };
+});
+
+export default reverseActions;

a26df17f626f2b4c82aecd656eed223b9a01cb54
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-25 23:44:51 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,10 +8,13 @@ const expectedFlat = fs.readFileSync(path.resolve(fixturesPath, 'expected.txt'),
 const expectedNested = fs.readFileSync(path.resolve(fixturesPath, 'expected-nested.txt'), 'utf8');
 
 const fixtures = [
-  [path.join(fixturesPath, 'before.json'), path.join(fixturesPath, 'after.json'), expectedFlat],
-  [path.join(fixturesPath, 'before.yml'), path.join(fixturesPath, 'after.yml'), expectedFlat],
-  [path.join(fixturesPath, 'before.ini'), path.join(fixturesPath, 'after.ini'), expectedFlat],
-  [path.join(fixturesPath, 'before-nested.json'), path.join(fixturesPath, 'after-nested.json'), expectedNested],
+  [path.join(fixturesPath, 'json', 'before.json'), path.join(fixturesPath, 'json', 'after.json'), expectedFlat],
+  [path.join(fixturesPath, 'json', 'before-nested.json'), path.join(fixturesPath, 'json', 'after-nested.json'), expectedNested],
+
+  [path.join(fixturesPath, 'yml', 'before.yml'), path.join(fixturesPath, 'yml', 'after.yml'), expectedFlat],
+  [path.join(fixturesPath, 'yml', 'before-nested.yml'), path.join(fixturesPath, 'yml', 'after-nested.yml'), expectedNested],
+
+  [path.join(fixturesPath, 'ini', 'before.ini'), path.join(fixturesPath, 'ini', 'after.ini'), expectedFlat],
 ];
 
 describe('genDiff', () => {

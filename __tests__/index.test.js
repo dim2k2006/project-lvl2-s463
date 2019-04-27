a02e379520d2b2156bf3f0bacc202c724a8fc3aa
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-27 12:25:07 +0300

    Add "plainFormatter" function

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,33 @@
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
+import formatTypes from '../src/types/formatTypes';
 
 const fixturesPath = path.join('.', '__tests__', '__fixtures__');
 
 const expectedFlat = fs.readFileSync(path.resolve(fixturesPath, 'expected.txt'), 'utf8');
+const expectedFlatPlain = fs.readFileSync(path.resolve(fixturesPath, 'expected-plain.txt'), 'utf8');
 const expectedNested = fs.readFileSync(path.resolve(fixturesPath, 'expected-nested.txt'), 'utf8');
 
 const fixtures = [
-  [path.join(fixturesPath, 'json', 'before.json'), path.join(fixturesPath, 'json', 'after.json'), expectedFlat],
-  [path.join(fixturesPath, 'json', 'before-nested.json'), path.join(fixturesPath, 'json', 'after-nested.json'), expectedNested],
+  // JSON default format
+  [path.join(fixturesPath, 'json', 'before.json'), path.join(fixturesPath, 'json', 'after.json'), formatTypes.DEFAULT, expectedFlat],
+  [path.join(fixturesPath, 'json', 'before-nested.json'), path.join(fixturesPath, 'json', 'after-nested.json'), formatTypes.DEFAULT, expectedNested],
 
-  [path.join(fixturesPath, 'yml', 'before.yml'), path.join(fixturesPath, 'yml', 'after.yml'), expectedFlat],
-  [path.join(fixturesPath, 'yml', 'before-nested.yml'), path.join(fixturesPath, 'yml', 'after-nested.yml'), expectedNested],
+  // JSON plain format
+  [path.join(fixturesPath, 'json', 'before.json'), path.join(fixturesPath, 'json', 'after.json'), formatTypes.PLAIN, expectedFlatPlain],
 
-  [path.join(fixturesPath, 'ini', 'before.ini'), path.join(fixturesPath, 'ini', 'after.ini'), expectedFlat],
-  [path.join(fixturesPath, 'ini', 'before-nested.ini'), path.join(fixturesPath, 'ini', 'after-nested.ini'), expectedNested],
+  // YML default format
+  [path.join(fixturesPath, 'yml', 'before.yml'), path.join(fixturesPath, 'yml', 'after.yml'), formatTypes.DEFAULT, expectedFlat],
+  [path.join(fixturesPath, 'yml', 'before-nested.yml'), path.join(fixturesPath, 'yml', 'after-nested.yml'), formatTypes.DEFAULT, expectedNested],
+
+  // INI default format
+  [path.join(fixturesPath, 'ini', 'before.ini'), path.join(fixturesPath, 'ini', 'after.ini'), formatTypes.DEFAULT, expectedFlat],
+  [path.join(fixturesPath, 'ini', 'before-nested.ini'), path.join(fixturesPath, 'ini', 'after-nested.ini'), formatTypes.DEFAULT, expectedNested],
 ];
 
 describe('genDiff', () => {
-  test.each(fixtures)('Should return correct diff. Index of the test case: %#', (before, after, expected) => {
-    expect(genDiff({ path1: before, path2: after })).toBe(expected);
+  test.each(fixtures)('Should return correct diff. Index of the test case: %#', (path1, path2, format, expected) => {
+    expect(genDiff({ path1, path2, format })).toBe(expected);
   });
 });
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,4 +1,4 @@
-const indentationChar = ' ';
+import actionTypes from '../types/actionTypes';
 
 /**
  * Converts ast to string
@@ -6,19 +6,47 @@ const indentationChar = ' ';
  * @param {Number} depth
  * @returns {String}
  */
-const plainFormatter = (ast, depth = 2) => {
-  const result = ast
-    .map(({
-      action,
-      key,
-      value,
-      children = [],
-    }) => `${indentationChar.repeat(depth)}${action} ${key}: ${(!children.length) ? value : plainFormatter(children, depth + 4)}`)
+const plainFormatter = (ast) => {
+  const result = ast.reduce((accumulator, {
+    action,
+    key,
+    value,
+    children = [],
+  }) => {
+    if (accumulator.find(item => item.key === key)) return accumulator;
+
+    if (action === actionTypes.DEFAULT) return accumulator;
+
+    const siblingItem = ast.find(item => item.key === key && item.action !== action);
+
+    if (action === actionTypes.ADDITION && !siblingItem) return [...accumulator, { key, message: `Property '${key}' was added with value: ${value}` }];
+
+    if (action === actionTypes.SUBTRACTION && !siblingItem) return [...accumulator, { key, message: `Property '${key}' was removed` }];
+
+    const oldValue = (action === actionTypes.ADDITION) ? siblingItem.value : value;
+    const newValue = (action === actionTypes.ADDITION) ? value : siblingItem.value;
+
+    return [...accumulator, { key, message: `Property '${key}' was updated. From ${oldValue} to ${newValue}` }];
+  }, [])
+    .map(item => item.message)
     .join('\n');
 
-  return `{
-${result}
-${indentationChar.repeat((depth > 2) ? depth - 2 : 0)}}`;
+  console.log('result:', result);
+
+  return result;
+
+//   const result = ast
+//     .map(({
+//       action,
+//       key,
+//       value,
+//       children = [],
+//     }) => `${indentationChar.repeat(depth)}${action} ${key}: ${(!children.length) ? value : plainFormatter(children, depth + 4)}`)
+//     .join('\n');
+//
+//   return `{
+// ${result}
+// ${indentationChar.repeat((depth > 2) ? depth - 2 : 0)}}`;
 };
 
 export default plainFormatter;

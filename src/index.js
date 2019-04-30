60daba5ab1d53d55bdc35b0179b79db3f5f3824c
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-30 21:29:06 +0300

    Update "genDiff" function. Remove "flow" chains.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import flow from 'lodash/flow';
 import getParser from './parsers';
 import buildAst from './utils/buildAst';
 import reverseAndUnique from './utils/reverseAndUnique';
@@ -10,99 +9,31 @@ import formatTypes from './types/formatTypes';
 import withPath from './utils/withPath';
 
 /**
- * Reads files
+ * Generates diff between two files
  * @param {Object} props
- * @returns {Object}
+ * @returns {String}
  */
-const readFiles = (props) => {
-  const { path1, path2 } = props;
+const genDiff = (props = {}) => {
+  const { path1, path2, format = formatTypes.DEFAULT } = props;
 
-  return {
-    ...props,
-    file1: fs.readFileSync(path.resolve(path1), 'utf8'),
-    file2: fs.readFileSync(path.resolve(path2), 'utf8'),
-    ext1: path.extname(path1).slice(1),
-    ext2: path.extname(path2).slice(1),
-  };
-};
+  const file1 = fs.readFileSync(path.resolve(path1), 'utf8');
+  const file2 = fs.readFileSync(path.resolve(path2), 'utf8');
 
-/**
- * Parses files
- * @param {Object} props
- * @returns {Object}
- */
-const parseFiles = (props) => {
-  const {
-    ext1,
-    ext2,
-    file1,
-    file2,
-  } = props;
+  const ext1 = path.extname(path1).slice(1);
+  const ext2 = path.extname(path2).slice(1);
 
-  return {
-    ...props,
-    data1: getParser(ext1)(file1),
-    data2: getParser(ext2)(file2),
-  };
-};
+  const data1 = getParser(ext1)(file1);
+  const data2 = getParser(ext2)(file2);
 
-/**
- * Retrieves ast for each file
- * @param {Object} props
- * @returns {Object}
- */
-const retrieveAst = (props) => {
-  const { data1, data2 } = props;
-
-  return {
-    ...props,
-    ast1: buildAst(data1),
-    ast2: buildAst(data2),
-  };
-};
-
-/**
- * Retrieves diff between two ast
- * @param {Object} props
- * @returns {Object}
- */
-const compareAst = (props) => {
-  const { ast1, ast2 } = props;
+  const ast1 = buildAst(data1);
+  const ast2 = buildAst(data2);
 
   const diff1 = getDiff(ast1, ast2);
   const diff2 = reverseAndUnique(diff1)(getDiff(ast2, ast1));
 
   const diff = [...diff1, ...diff2];
 
-  return {
-    ...props,
-    diff,
-  };
-};
-
-/**
- * Renders the ast
- * @param {Object} props
- * @returns {String}
- */
-const renderAst = (props) => {
-  const { diff, format = formatTypes.DEFAULT } = props;
-
   return `${getFormatter(format)(withPath(diff))}\n`;
 };
-
-/**
- * Generates diff between two files
- * @param {String} pathToFile1
- * @param {String} pathToFile2
- */
-const genDiff = flow(
-  readFiles,
-  parseFiles,
-  retrieveAst,
-  compareAst,
-  renderAst,
-);
-
 
 export default genDiff;

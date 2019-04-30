4001939d601320952e3cf1628c442d75c9c51a0e
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-04-30 21:46:02 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,23 +8,21 @@ describe('genDiff', () => {
   const dataTypes = ['json', 'yml', 'ini'];
 
   const table = dataTypes.reduce((accumulator, dataType) => {
-    const testRow = Object.values(formatTypes).reduce((iAccumulator, formatType) => {
-      return [
-        ...iAccumulator,
-        [
-          path.join(fixturesPath, dataType, `before.${dataType}`),
-          path.join(fixturesPath, dataType, `after.${dataType}`),
-          formatType,
-          fs.readFileSync(path.join(fixturesPath, `expected-${formatType}.txt`), 'utf-8'),
-        ],
-        [
-          path.join(fixturesPath, dataType, `before-nested.${dataType}`),
-          path.join(fixturesPath, dataType, `after-nested.${dataType}`),
-          formatType,
-          fs.readFileSync(path.join(fixturesPath, `expected-nested-${formatType}.txt`), 'utf-8'),
-        ],
-      ];
-    }, []);
+    const testRow = Object.values(formatTypes).reduce((iAccumulator, formatType) => ([
+      ...iAccumulator,
+      [
+        path.join(fixturesPath, dataType, `before.${dataType}`),
+        path.join(fixturesPath, dataType, `after.${dataType}`),
+        formatType,
+        fs.readFileSync(path.join(fixturesPath, `expected-${formatType}.txt`), 'utf-8'),
+      ],
+      [
+        path.join(fixturesPath, dataType, `before-nested.${dataType}`),
+        path.join(fixturesPath, dataType, `after-nested.${dataType}`),
+        formatType,
+        fs.readFileSync(path.join(fixturesPath, `expected-nested-${formatType}.txt`), 'utf-8'),
+      ],
+    ]), []);
 
     return [
       ...accumulator,

6875fb18017b42f787059e249b04fefa07055df8
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-05-27 20:55:26 +0100

    Update "genDiff" function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 import getParser from './parsers';
-import buildAst from './utils/buildAst';
-import reverseAndUnique from './utils/reverseAndUnique';
-import getDiff from './utils/getDiff';
 import getFormatter from './formatters';
 import formatTypes from './types/formatTypes';
-import withPath from './utils/withPath';
+
+import utils from './utils';
+
+const { getKeys, getDiff, withPath } = utils;
 
 /**
  * Generates diff between two files
@@ -15,22 +15,18 @@ import withPath from './utils/withPath';
  * @returns {String}
  */
 const genDiff = (filePaths = [], format = formatTypes.DEFAULT) => {
-  const trees = filePaths
+  const data = filePaths
     .map((filepath) => {
       const content = fs.readFileSync(path.resolve(filepath), 'utf8');
       const fileExtension = path.extname(filepath).slice(1);
-      const data = getParser(fileExtension)(content);
-      const ast = buildAst(data);
-
-      return ast;
+      return getParser(fileExtension)(content);
     });
 
-  const [ast1, ast2] = trees;
+  const [data1, data2] = data;
 
-  const diff1 = getDiff(ast1, ast2);
-  const diff2 = reverseAndUnique(diff1)(getDiff(ast2, ast1));
+  const keys = getKeys(data1, data2);
 
-  const diff = [...diff1, ...diff2];
+  const diff = getDiff(keys, data1, data2);
 
   return `${getFormatter(format)(withPath(diff))}\n`;
 };

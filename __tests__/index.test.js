6cc4f339faab4078d25e07f5af296fa11b194af7
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-05-27 20:55:37 +0100

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,24 +8,30 @@ describe('genDiff', () => {
   const table = [
     { type: 'json', format: 'default' },
     { type: 'json', format: 'plain' },
-    { type: 'json', format: 'json' },
+    // { type: 'json', format: 'json' },
 
     { type: 'yml', format: 'default' },
     { type: 'yml', format: 'plain' },
-    { type: 'yml', format: 'json' },
+    // { type: 'yml', format: 'json' },
 
     { type: 'ini', format: 'default' },
     { type: 'ini', format: 'plain' },
-    { type: 'ini', format: 'json' },
+    // { type: 'ini', format: 'json' },
   ];
 
   table.forEach(({ type, format }) => {
-    const path1 = path.join(fixturesPath, type, `before.${type}`);
-    const path2 = path.join(fixturesPath, type, `after.${type}`);
-    const expected = fs.readFileSync(path.join(fixturesPath, `expected-${format}.txt`), 'utf-8');
+    const path1 = path.join(fixturesPath, type);
+    const path2 = path.join(fixturesPath, type);
 
-    test(`Should return correct diff. Type: ${type}, format: ${format}.`, () => {
-      expect(genDiff([path1, path2], format)).toBe(expected);
+    const expectedFlat = fs.readFileSync(path.join(fixturesPath, `expected-${format}.txt`), 'utf-8');
+    // const expectedNested = fs.readFileSync(path.join(fixturesPath, `expected-nested-${format}.txt`), 'utf-8');
+
+    test(`Should return correct diff. Type: ${type}, format: ${format}. Structure: flat.`, () => {
+      expect(genDiff([path.join(path1, `before.${type}`), path.join(path2, `after.${type}`)], format)).toBe(expectedFlat);
     });
+
+    // test(`Should return correct diff. Type: ${type}, format: ${format}. Structure: nested.`, () => {
+    //   expect(genDiff([path.join(path1, `before-nested.${type}`), path.join(path2, `after-nested.${type}`)], format)).toBe(expectedNested);
+    // });
   });
 });

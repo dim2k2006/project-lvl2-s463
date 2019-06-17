0c21d98eae276872d06e1021fb924481581b415a
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-06-17 21:46:41 +0300

    Update complexFormatter

--- src/formatters/complex.js
+++ src/formatters/complex.js
@@ -1,24 +1,20 @@
 const indentationChar = ' ';
 
-const typeMapper = {
-  added: '+',
-  removed: '-',
-  unchanged: ' ',
+const nodeTypes = {
+  added: node => `+ ${node.key}: ${node.newValue}`,
+  removed: node => `- ${node.key}: ${node.oldValue}`,
+  unchanged: node => `  ${node.key}: ${node.newValue}`,
+  changed: node => `+ ${node.key}: ${node.newValue}\n  - ${node.key}: ${node.oldValue}`,
 };
 
 const complexFormatter = (ast, depth = 2) => {
-  const result = ast
-    .map(({
-      type,
-      key,
-      value,
-      children = [],
-    }) => `${indentationChar.repeat(depth)}${typeMapper[type]} ${key}: ${(!children.length) ? value : complexFormatter(children, depth + 4)}`)
+  const string = ast
+    .map(node => `${indentationChar.repeat(depth)}${nodeTypes[node.type](node)}`)
     .join('\n');
 
   return `{
-${result}
-${indentationChar.repeat((depth > 2) ? depth - 2 : 0)}}`;
+${string}
+}`;
 };
 
 export default complexFormatter;
